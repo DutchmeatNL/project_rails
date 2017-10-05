db4051e74f42f5d0d889c406f23246a2f41342e7
Jeroen de Zeeuw <yerouen@hotmail.com>
2017-10-05 13:53:15 +0200

    Sorcery authentication complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,9 @@ class TagsController < ApplicationController
    def index
       @tags = Tag.all
    end
-      
+   
+   before_action  :require_login, only: [:destroy]
+   
    def show
       @tag = Tag.find(params[:id])
    end
